688a6e5601d87c9e75e9f75815379cec0d2c95ff
olalekan <lekanmojibola@gmail.com>
2024-04-14 17:15:39 +0100

    made a copy of the BaseModel module

--- test_base_model.py
+++ test_base_model.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""Doc
+"""
+from models.tmp_base_model import *
+from models.tmp_base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model.id)
-print("-------")
-print(my_model)
-print("-------")
-my_model.save()
-print(my_model)
-print("-------")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("-------")
-print(type(my_model.created_at))
-print("-------")
 
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class BaseModel(BaseModel):
+    """Doc
+    """
 
+    def save(self):
+        """Doc
+        """
+        pass
